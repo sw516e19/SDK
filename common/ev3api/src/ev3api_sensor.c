1f6cbea236bfca23ff99203a7ef03f34122927ed
Rasmus Lindegaard <rasmusnielsen91@gmail.com>
2019-09-24 14:47:20 +0200

    pixycam can track 2 objects, but the i2c protocol has a 32 byte max read we need to circumvent

--- common/ev3api/src/ev3api_sensor.c
+++ common/ev3api/src/ev3api_sensor.c
@@ -551,7 +551,10 @@ void pixycam_2_get_blocks(sensor_port_t port, pixycam_2_block_response *dest, ui
 	CHECK_COND(ev3_sensor_get_type(port) == PIXYCAM_2, E_OBJ);
 	CHECK_COND(*pI2CSensorData[port].status == I2C_TRANS_IDLE, E_OBJ);
 
-	ercd = start_i2c_transaction(port, 0x54, &req, sizeof(pixycam2_request_get_blocks), sizeof(pixycam_2_block) * blocks);//"\xAE\xC1\xE\x00", 4, 13); //
+	ercd = start_i2c_transaction(port, 0x54, &req, sizeof(pixycam2_request_get_blocks), 32);
+	//TODO: Well, the actual size should be: '(sizeof(pixycam2_header) + sizeof(short)) + (sizeof(pixycam_2_block) * blocks)'
+	//but due to i2c limitations, the max payload is 32 bytes (But sadly that's including the header)
+	//"\xAE\xC1\xE\x00", 4, 13); //
 
 	assert(ercd == E_OK);
 
--- workspace/pixycam-test/app.c
+++ workspace/pixycam-test/app.c
@@ -48,10 +48,67 @@ void main_task(intptr_t unused) {
         ev3_lcd_draw_string(coord_buf, 0, 14);
         ev3_lcd_draw_string(h_buf, 0, 28);
         ev3_lcd_draw_string(w_buf, 0, 42);
-*/        
+     */  
     }
 
+    //pixycam_2_uart_test();
 
     ev3_lcd_draw_string("Done         ", 0, EV3_LCD_HEIGHT/2);
 
 }
+
+typedef struct{
+
+    pixycam2_header header;
+	uint8_t signature;
+	uint8_t blocks;
+
+} pixy_req;
+
+void pixycam_2_uart_test(){
+
+    FILE *file;
+    file = ev3_serial_open_file(EV3_SERIAL_UART);
+
+    pixycam_2_block blocks[3];
+    pixycam_2_block_response response;
+    
+    response.blocks = blocks;
+
+    pixy_req req;  
+    req.header.sync = 0xc1ae;
+	req.header.packet_type = 32;
+	req.header.payload_length = 2;
+	req.signature = 255;
+	req.blocks = 3;
+
+    char block_1_coords[20];
+    char block_2_coords[20];
+    unsigned long s = sizeof(pixycam2_header) + sizeof(short) + (sizeof(pixycam_2_block) * 3);
+    uint8_t buffer[s];
+
+    while(1){
+        ev3_lcd_draw_string("1", 0, 0);
+        fwrite(&req, sizeof(uint8_t), sizeof(pixy_req), file);
+        fseek(file, 0, SEEK_SET);
+        tslp_tsk(20);
+        ev3_lcd_draw_string("2", 0, 0);
+        fread(&buffer, sizeof(uint8_t), s, file);
+        ev3_lcd_draw_string("3", 0, 0);
+        unsigned int val = response.header.payload_length == 0 ? 0 : (response.header.payload_length / 14);
+            //sprintf(&payload_buf, "%u    ", val);
+
+        if(val > 0){
+            sprintf(&block_1_coords, "co: %u,%u s: %d     ", response.blocks[0].x_center, response.blocks[0].y_center, blocks[0].signature);
+            ev3_lcd_draw_string(block_1_coords, 0, 16);
+        }
+
+        /*if(val > 1){
+            sprintf(&block_2_coords, "co: %u %u s: %d     ", blocks[1].x_center, blocks[1].y_center, blocks[1].signature);
+            ev3_lcd_draw_string(block_2_coords, 0, 32);
+        }*/
+    }
+    
+
+}
+
