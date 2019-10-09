ffdf54a2d6df3d367c7166357e331f4f825c430b
Rasmus Lindegaard <rasmusnielsen91@gmail.com>
2019-10-09 08:59:44 +0200

    Tracking an object while pressing the sensor works

--- workspace/cannon-alpha/app.c
+++ workspace/cannon-alpha/app.c
@@ -1,26 +1,22 @@
 #include "app.h"  
 
+void fire(){
+    ev3_motor_rotate(EV3_PORT_A, - 360, 100, 0);
+}
+
 void main_task(intptr_t unused){
 
     ev3_lcd_set_font(EV3_FONT_MEDIUM);
 
     ev3_motor_config(EV3_PORT_A, LARGE_MOTOR);
 
-    int32_t t = ev3_motor_get_counts(EV3_PORT_A);
+    bool_t stop = 0;
+
+    ev3_button_set_on_clicked(BACK_BUTTON, fire, 0);
+
+    while(1);
     
-    ev3_motor_rotate(EV3_PORT_A, -1, 100, 1);
-
-    int32_t k = ev3_motor_get_counts(EV3_PORT_A);
-
-    char buff[20];
-
-    sprintf(&buff, "Pos: %d", t);
-
-    ev3_lcd_draw_string(buff, 0, 0);
-
-    sprintf(&buff, "Pos: %d", k);
-
-    ev3_lcd_draw_string(buff, 0, 18);
 
 }
+
 
