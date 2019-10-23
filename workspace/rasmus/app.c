d26ed313663e95b53cc17d088f44ad49638431d1
Jonas Andersen <jonas@anrsn.me>
2019-10-23 12:42:04 +0200

    Count falling obj

--- workspace/rasmus/app.c
+++ workspace/rasmus/app.c
@@ -49,7 +49,7 @@ void measure_brick(block_signature_t signature){
         pixycam_2_get_blocks(pixy_port, &response, signature, 1);
 
         if(response.header.payload_length > 0 ){
-            sprintf(&buffer, "W: %u | H: %u ", block[0].width, block[0].height);
+            sprintf(buffer, "W: %u | H: %u ", block[0].width, block[0].height);
             ev3_lcd_draw_string(buffer, 0, EV3_LCD_HEIGHT / 2);
             tslp_tsk(1000);
         }
@@ -58,8 +58,75 @@ void measure_brick(block_signature_t signature){
 
 }
 
+void count_colors() {
+
+    block_signature_t signature = SIGNATURE_1;
+    SYSTIM start, end, prev_img, curr_img;
+    uint8_t blocks_detected = 0;
+    uint8_t pictures_taken = 0;
+    char str[16][40];
+
+    for (uint8_t i = 0; i < 16; i++)
+    {
+        sprintf(str[i], "                                        ");
+    }
+    
+
+    pixycam2_block_response_t response;
+    pixycam2_block_t block[1];
+    response.blocks = &block;
+    bool found = false;
+    ev3_led_set_color(LED_RED);
+    tslp_tsk(400);
+    while(!found) {
+        pixycam_2_get_blocks(EV3_PORT_1, &response, signature, 1);
+        found = response.header.payload_length == 14;
+    }
+    ev3_led_set_color(LED_ORANGE);
+    get_tim(&start);
+    end = start;
+    prev_img = start;
+    while(end - start < 400) {
+        pixycam_2_get_blocks(EV3_PORT_1, &response, signature, 1);
+        pictures_taken++;
+        if (response.header.payload_length == 14) {
+            if(response.blocks[0].signature == 1){
+                blocks_detected++;
+                if(blocks_detected > 0 && blocks_detected < 15) {
+                    get_tim(&curr_img);
+                    sprintf(str[blocks_detected + 1], "%i X:%u Y:%u %lu   ", blocks_detected, response.blocks[0].x_center, response.blocks[0].y_center, curr_img - prev_img);
+                    prev_img = curr_img;
+                }
+                response.blocks[0].signature = 0;
+                response.header.payload_length = 0;
+            }
+        }
+        get_tim(&end);
+    }
+    ev3_led_set_color(LED_OFF);
+
+    sprintf(str[0], "Pics taken: %u      ", pictures_taken);
+    sprintf(str[1], "Count: %i           ", blocks_detected);
+    for (int8_t i = 0; i < 16; i++)
+    {
+        ev3_lcd_draw_string(str[i], 0, i*8);
+    }
+}
+
+void setup_count(block_signature_t signature) {
+
+
+    ev3_sensor_config(EV3_PORT_1, PIXYCAM_2);
+
+    ev3_lcd_set_font(EV3_FONT_SMALL);
+    ev3_button_set_on_clicked(DOWN_BUTTON, count_colors, 0);
+    while(1) {
+
+    }
+
+}
+
 void main_task(intptr_t unused) {
 
-    measure_brick(SIGNATURE_1);
-
+    setup_count(SIGNATURE_1);
 }
