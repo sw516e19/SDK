a92f90fb25f0bedcfe2df790d100183976adbdc8
Rasmus Lindegaard <rasmusnielsen91@gmail.com>
2019-10-31 12:06:37 +0100

    task-test works. remember to loop all tasks that you desire to be kept aLIIIIIIIVE!

--- workspace/cannon/app.c
+++ workspace/cannon/app.c
@@ -19,9 +19,25 @@
 #define _debug(x)
 #endif
 
+void write_string(const char *arr, bool_t top) {
+        ev3_lcd_set_font(EV3_FONT_MEDIUM);
+
+    int y = top ? 0 : EV3_LCD_HEIGHT / 2;
+    ev3_lcd_draw_string(arr, 0, y);
+}
+
+int write = 0;
 
 // Detect an object with the PixyCam and write the block to a buffer to be further processed
 void detect_task(intptr_t unused) {
+
+    while (true)
+    {
+        ++write;
+
+        write_string("DETECT_TASK", true);
+        tslp_tsk(1000);
+    }
 
     // Get the block of the falling object. Write it to a data structure that calculate_task can read
     // TODO: REWRITE DIS
@@ -30,6 +46,19 @@ void detect_task(intptr_t unused) {
 
 // Perform calculations on the data that the pixycam detected, and estimate when to shoot the target
 void calculate_task(intptr_t unused) {
+
+    char buff[30];
+
+    while (true)
+    {
+
+        sprintf(&buff, "CALCULATE_TASK %d", write);
+
+        write_string(buff, false);
+
+        tslp_tsk(100);
+    }
+
 
     // Read data from detect_task, perform calculations and eventually pass shooting request to shoot_task
 
