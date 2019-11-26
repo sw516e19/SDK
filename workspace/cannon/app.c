9074476bb4b3912c1d6ed02ba839ed59b9242575
Jonas Andersen <jonas@anrsn.me>
2019-11-26 16:11:01 +0100

    UTM time

--- workspace/cannon/app.c
+++ workspace/cannon/app.c
@@ -11,12 +11,27 @@
 
 #include <math.h>
 
-#define GRAVITY_PIXELS 0.002804285714285714 // Measured in pixels/ms
+/**
+ * Fall length = tan(20) * 1 meter * 2 (2 pices) 
+ * Resolution = Fall length / 208 pixel = 0.0035 meter/pixel
+ * Gravityacceleration: 9.815 m/s^2 / 0.0035 m/pixel = 2804 pixel/s^2
+ * Recalc to milliseconds = 2804 p/s^2 * 1/1000 s/ms * 1/1000 s/ms = 0.00280428571428571428571428571429 pixel/ms^2
+ * Recalc to microseconds = 2804 p/s^2 * 1/1000000 s/µs * 1/1000000 s/µs = 2.8042857142857142857142857142857E-9
+ */
+const double GRAVITY_PIXELS = 2.804285e-09; // Measured in pixels/microsecond. Fomular: (tan(20) * 1 * 2) / 208 = 0.0035 m/pixel. 9.815 m/s^2 / 0.0035 m/pixel = 2804 pixel/second
 #define PIXYCAM_RESPONSE_THRESHOLD 5 // The maximum amount of responses that we want to retrieve
 #define PIXYCAM_BLOCK_THRESHOLD 1 // The maximum amount of pixycam blocks we want to retrieve in the response
-#define POINT_OF_IMPACT 281 // the calculated point of impact at a distance of a meter
+/**
+ * Resolution = 0.0035 m/pixel
+ * Distance from camera to shooter = 0.66 m
+ * Drop in flight = 0.09 m
+ * Falltotal = (0.66 m + 0.09 m) / 0.0035 m/pixel = 214 pixel
+ * Offset = 208 / 2 = 104 pixel
+ * Total = 214 pixel + 104 pixel = 318 pixel 
+ */
+#define POINT_OF_IMPACT 318 // the calculated point of impact at a distance of a meter
 #define GEARING 5
-#define PROJECTILE_TRAVEL_TIME 135
+#define PROJECTILE_TRAVEL_TIME 135 * 1000 //Microseconds
 
 // Enable debugging
 #define DEBUG
@@ -24,8 +39,8 @@
 // Enable WCRTA (Worst-case response time analysis)
 #define WCRTA
 
-// Global variable for trigger time
-uint8_t trigger_time = 90;
+    // Global variable for trigger time in microseconds
+    uint8_t trigger_time = 90 * 1000;
 
 // Function to modify the trigger time
 void modify_trigger_time(uint8_t *change) {
@@ -55,12 +70,12 @@ void init_task(intptr_t unused) {
 // The struct of the detected pixycam block response. Contains the detection time and current block index to be parsed by calculate_task.
 typedef struct {
     uint16_t y;
-    SYSTIM timestamp;
+    SYSUTM timestamp;
 } detected_pixycam_block_t;
 
 // Global variable for the block that was detected
 detected_pixycam_block_t detected_blocks[CAMDATAQUEUESIZE + 1];
-SYSTIM calculated_deadlines[CALCDATAQUEUESIZE + 1];
+SYSUTM calculated_deadlines[CALCDATAQUEUESIZE + 1];
 
 // Detect an object with the PixyCam and write the block to a buffer to be further processed
 void detect_task(intptr_t unused) {
@@ -81,7 +96,7 @@ void detect_task(intptr_t unused) {
 
     pixycam_response.header.payload_length = 0;
 #ifdef WCRTA
-    SYSTIM startTime, endTime;
+    SYSUTM startTime, endTime;
 #endif
 
 #ifdef DEBUG
@@ -97,7 +112,7 @@ void detect_task(intptr_t unused) {
         while (pixycam_2_fetch_blocks(EV3_PORT_1, &pixycam_response, 1) == 0)
             tslp_tsk(2);
 #ifdef WCRTA
-        get_tim(&startTime);
+        get_utm(&startTime);
 #endif        
 
         // If the payload length is 0, no block(s) were detected and the loop should be continued
@@ -109,8 +124,12 @@ void detect_task(intptr_t unused) {
             continue;
 
         // Get the detection time
-        get_tim(&detected_blocks[index].timestamp); // Time: 17
+        get_utm(&detected_blocks[index].timestamp); // Time: 17
         detected_blocks[index].y = pixycam_response.blocks[0].y_center;
+#ifdef DEBUG
+        syslog(LOG_NOTICE, "B-Width: %d -> %d", pixycam_response.blocks[0].width, pixycam_response.blocks[0].width * 0.0035 * 100);
+        syslog(LOG_NOTICE, "B-Height: %d -> %d", pixycam_response.blocks[0].height, pixycam_response.blocks[0].height * 0.0035 * 100);
+#endif
 
         snd_dtq(CAMDATAQUEUE, &detected_blocks[index]);
         index++;
@@ -121,26 +140,46 @@ void detect_task(intptr_t unused) {
         pixycam_response.header.payload_length = 0;  
         pixycam_response.blocks[0].signature = -1;   
 #ifdef WCRTA
-        get_tim(&endTime);
+        get_utm(&endTime);
         syslog(LOG_NOTICE, "[WCRTA] dect: %lu", endTime - startTime);
 #endif
     }
 }
 
-int32_t calculate_fallduration(uint16_t *y_0_ptr, uint16_t *y_1_ptr, SYSTIM *y0_millis_ptr, SYSTIM *y1_millis_ptr) {
+int32_t calculate_fallduration(uint16_t *y0_location, uint16_t *y1_location, SYSUTM *y0_time, SYSUTM *y1_time) {
 
     // Dereference the values. POTENTIALLY TURN TO PASS-BY-VALUE, LOOK THROUGH LATER
-    uint16_t y_0 = *y_0_ptr;
-    uint16_t y_1 = *y_1_ptr;
-    int millis = *y1_millis_ptr - *y0_millis_ptr;
+    uint16_t y_0 = *y0_location;
+    uint16_t y_1 = *y1_location;
+    int32_t falltime = *y1_time - *y0_time;
 
-    //double milis_dec = millis / 1000; //is this actually necessary?
+    //double milis_dec = falltime / 1000; //is this actually necessary?
     
     // 1. Find average fall speed
-    double v_avg = (y_1 - y_0) / millis;
+    double v_avg = (y_1 - y_0) / falltime;
+
+    #ifdef DEBUG
+    syslog(LOG_NOTICE, "avg: %f");
+    #endif
 
     // 2. Find v_0 by subtracting the gained velocity in half the sample time.
-    double v_0 = v_avg - GRAVITY_PIXELS * millis * 0.5;
+    double v_0 = v_avg - GRAVITY_PIXELS * falltime * 0.5;
+#ifdef DEBUG
+    char str[11][30];
+    sprintf(str[10], "G: %.6e", GRAVITY_PIXELS);
+    syslog(LOG_NOTICE, str[10]);
+    syslog(LOG_NOTICE, "v_0: %f", v_0);
+
+    double number = 2.8042852e-4;
+    for (uint8_t i = 0; i < 10; i++)
+    {
+        sprintf(str[i], "G: %E", number);
+        syslog(LOG_NOTICE, str[i]);
+        number = number / 10;
+    }
+    
+    
+#endif
 
     // 3. Calculate the milliseconds needed to fall to the point of impact (POI) use rewrite of:
     // x = x_0 + v_0 * t + 0.5 * g * t² => t = (sqrt(2 a (y - x) + v^2) - v)/a and a!=0
@@ -159,7 +198,7 @@ void calculate_task(intptr_t unused) {
     uint16_t avgfalltime = 0;
     uint16_t offsetFromFirstDetect = 0;
     uint16_t count;
-    SYSTIM firstDetect;
+    SYSUTM firstDetect;
     uint8_t queue_index = 0;
     ER ercd;
 
@@ -169,19 +208,22 @@ void calculate_task(intptr_t unused) {
     intptr_t current_ptr;
     detected_pixycam_block_t *currentdata, *olddata = NULL;
 #ifdef WCRTA
-    SYSTIM startTime, endTime;
+    SYSUTM startTime, endTime;
 #endif
 
     while (true) {
         //TODO: Add timeout.
         rcv_dtq(CAMDATAQUEUE, &current_ptr);
 #ifdef WCRTA
-        get_tim(&startTime);
+        get_utm(&startTime);
 #endif
         
         currentdata = (detected_pixycam_block_t*)current_ptr;
 
         if(olddata == NULL) {
+#ifdef DEBUG
+            syslog(LOG_NOTICE, "RESET CALC: first shot");
+#endif
             firstDetect = currentdata->timestamp;
             olddata = currentdata;
             sumfall = 0;
@@ -189,7 +231,10 @@ void calculate_task(intptr_t unused) {
             continue;
         }
 
-        if(currentdata->timestamp - olddata->timestamp > 1000) {
+        if(currentdata->timestamp - olddata->timestamp > 1 * 1000 * 1000) {
+#ifdef DEBUG
+            syslog(LOG_NOTICE, "RESET CALC: > 1 second old");
+#endif
             firstDetect = currentdata->timestamp;
             olddata = currentdata;
             sumfall = 0;
@@ -224,7 +269,7 @@ void calculate_task(intptr_t unused) {
 
         olddata = currentdata;
 #ifdef WCRTA
-        get_tim(&endTime);
+        get_utm(&endTime);
         syslog(LOG_NOTICE, "[WCRTA] calc: %lu", endTime - startTime);
 #endif
 
@@ -249,31 +294,32 @@ void shoot_task(intptr_t unused) {
     syslog(LOG_NOTICE, "Shoot task init");
 #endif
 
-    SYSTIM now;
-    SYSTIM *new_data;
+    SYSUTM now;
+    SYSUTM *new_data;
+    
     intptr_t pointer;
-    SYSTIM time_to_shoot = 0;
+    SYSUTM time_to_shoot = 0;
     ER ercd;
     // Initialize the motor
     ev3_motor_config(EV3_PORT_A, LARGE_MOTOR);
 #ifdef WCRTA
-    SYSTIM startTime, endTime;
+    SYSUTM startTime, endTime;
 #endif
 
     while(true) {
         
         ercd = trcv_dtq(CALCDATAQUEUE, &pointer, 2);
 #ifdef WCRTA
-        get_tim(&startTime);
+        get_utm(&startTime);
 #endif
-        new_data = (SYSTIM *)pointer;
+        new_data = (SYSUTM *)pointer;
         if(ercd != E_TMOUT){
 #ifdef DEBUG
             syslog(LOG_NOTICE, "Setting new tts: %lu, Old time: %lu", *new_data, time_to_shoot);
 #endif
             time_to_shoot = *new_data;
         }
-        get_tim(&now); 
+        get_utm(&now); 
 
         if(time_to_shoot == 0) {
             continue;
@@ -284,7 +330,7 @@ void shoot_task(intptr_t unused) {
         ev3_motor_rotate(EV3_PORT_A, 360 * GEARING, 100, true);
         time_to_shoot = 0;
 #ifdef WCRTA
-        get_tim(&endTime);
+        get_utm(&endTime);
         syslog(LOG_NOTICE, "[WCRTA] shoo: %lu", endTime - startTime);
 #endif
         
